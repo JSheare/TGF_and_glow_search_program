9ff4e031f07c9326957cbc3d68f3111748554dcd
BuildTools <unconfigured@null.spigotmc.org>
2022-11-01 22:27:53 -0700

    start repo

---
+++ search_classes.py
@@ -0,0 +1,322 @@
+import glob
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import search_module as sm
+import DataReaderFinal as dr
+
+# Classes used in search.py
+
+
+# Class used to store all relevant information about the detector and the data
+class Detector:
+    def __init__(self, unit, day, modes):
+        self.unit = unit
+        self.day = day
+        self.modes = modes
+
+        self.THOR = False
+        self.GODOT = False
+        self.SANTIS = False
+
+        self.custom = False
+        self.processed = False
+        self.plastics = False
+        self.import_path = ''
+
+        if self.unit == 'GODOT':
+            self.GODOT = True
+            if 'processed' in self.modes:
+                self.processed = True
+                self.import_path = f'{sm.G_processed_data_loc()}{day[0:4]}/'
+            else:
+                self.import_path = f'{sm.G_raw_data_loc()}{day}/'
+
+            self.scintillators = {'NaI': {'eRC': '1490', 'filelist': [],
+                                          'time': np.array([]), 'energy': np.array([])},
+                                  'LP': {'eRC': '1491', 'filelist': [],
+                                         'time': np.array([]), 'energy': np.array([])}}
+
+        elif self.unit[0:4] == 'THOR':
+            self.THOR = True
+            self.import_path = f'{sm.T_raw_data_loc()}{unit}/Data/{day}/'
+            self.scintillators = {'NaI': {'eRC': sm.T_eRC(self.unit)[0], 'filelist': [],
+                                          'time': np.array([]), 'energy': np.array([])},
+                                  'SP': {'eRC': sm.T_eRC(self.unit)[1], 'filelist': [],
+                                         'time': np.array([]), 'energy': np.array([])},
+                                  'MP': {'eRC': sm.T_eRC(self.unit)[2], 'filelist': [],
+                                         'time': np.array([]), 'energy': np.array([])},
+                                  'LP': {'eRC': sm.T_eRC(self.unit)[3], 'filelist': [],
+                                         'time': np.array([]), 'energy': np.array([])}}
+
+        elif self.unit == 'SANTIS':
+            self.SANTIS = True
+            self.import_path = f'{sm.S_raw_data_loc()}{day}/'
+            self.scintillators = {'LP': {'eRC': '2549', 'filelist': [],
+                                         'time': np.array([]), 'energy': np.array([])}}
+        else:
+            print('Not a valid detector')
+            exit()
+
+        if 'custom' in self.modes:
+            self.custom = True  # Redundant
+            self.import_path = f'{sm.C_raw_data_loc()}'
+
+        if 'processed' in self.modes:
+            self.processed = True
+            if self.unit != 'GODOT':
+                print('Processed data is only accessible for GODOT')
+                exit()
+
+        if 'plastics' in self.modes:
+            self.plastics = True
+
+    # Retrieves the requested attribute for a particular scintillator
+    def attribute_retriever(self, scintillator, attribute):  # Make it possible to request multiple attributes at once?
+        medium = self.scintillators[scintillator]
+        desired_attribute = medium[attribute]
+        return desired_attribute
+
+    # Combines the time and energy arrays for multiple scintillators
+    def scintillator_combiner(self, *args):
+        time = np.array([])
+        energy = np.array([])
+        iterations = 0
+        for arg in args:
+            new_time = self.attribute_retriever(arg, 'time')
+            new_energy = self.attribute_retriever(arg, 'energy')
+            if iterations == 0:
+                time = new_time
+                energy = new_energy
+            else:
+                time = np.append(time, new_time)
+                sorting_order = np.argsort(time)
+                time = np.sort(time)
+                energy = np.append(energy, new_energy)
+                energy = energy[sorting_order.astype(int)]
+
+        return time, energy
+
+    # Imports data from datafiles into arrays
+    def data_importer(self, datetime_logs):
+        for i in self.scintillators:
+            scintillator = self.scintillators[i]
+            eRC = scintillator['eRC']
+            sm.print_logger(f'\nFor eRC {eRC} ({i}):', datetime_logs)
+            if self.THOR:
+                complete_filelist = glob.glob(f'{self.import_path}eRC{eRC}*_lm_{self.day}_*')
+            else:
+                complete_filelist = glob.glob(f'{self.import_path}eRC{eRC}_lm4_*_{self.day}_*')
+
+            # Filters out trace mode files and .txtp files (whatever those are)
+            filtered_filelist = []
+            unfiltered_filelist = []
+            for u in range(len(complete_filelist)):
+                current_file = complete_filelist[u]
+                if current_file[-3:] == 'xtr' or current_file[-4:] == 'txtp' or current_file[-5:] == 'xtrpp':
+                    continue
+                elif current_file[-7:] == '.txt.gz':
+                    filtered_filelist.append(current_file)
+                else:
+                    unfiltered_filelist.append(current_file)
+
+            # Eliminates duplicate files
+            for h in range(len(unfiltered_filelist)):
+                current_file = unfiltered_filelist[h]
+                if f'{current_file}.gz' in filtered_filelist:
+                    continue
+                else:
+                    filtered_filelist.append(current_file)
+
+            # Puts the files in the correct order
+            files = []
+            extensions = []
+            for ik in range(len(filtered_filelist)):
+                file = filtered_filelist[ik]
+                if file[-4:] == '.txt':
+                    files.append(file.replace('.txt', ''))
+                    extensions.append('.txt')
+                elif file[-4:] == '.csv':
+                    files.append(file.replace('.csv', ''))
+                    extensions.append('.csv')
+                elif file[-7:] == '.txt.gz':
+                    files.append(file.replace('.txt.gz', ''))
+                    extensions.append('.txt.gz')
+
+            file_order = np.argsort(files)
+            files.sort()
+            extensions = [extensions[s] for s in file_order]
+            filelist = []
+            for it in range(len(files)):
+                filelist.append(f'{files[it]}{extensions[it]}')
+
+            try:
+                # Tests to make sure that new_filelist isn't empty
+                assert len(filelist) > 0, 'No files for this scintillator today'
+
+                # Starts actually importing the data
+                energy_list = []
+                time_list = []
+
+                filetimes = np.array([])
+                file_time_gaps = np.array([])
+                last_second = 0.0
+                files_imported = 0
+
+                print('File|File Behavior|File Time Gap (sec)', file=datetime_logs)
+                for day_files in filelist:
+                    # Try-except block to log files where GPS and wallclock disagree significantly
+                    file_behavior = 'Normal'
+                    try:
+                        if self.processed:
+                            e, t = np.loadtxt(day_files, skiprows=1, usecols=(0, 2), unpack=True)
+                            energy_list.append(e)
+                            time_list.append(t)
+                            filetimes = t
+                        else:
+                            data = dr.fileNameToData(day_files)
+                            if 'energies' in data.columns:
+                                energy_list.append(data['energies'].to_numpy())
+                            else:
+                                energy_list.append(data['energy'].to_numpy())
+
+                            time_list.append(data['SecondsOfDay'].to_numpy())
+                            filetimes = data['SecondsOfDay'].to_numpy()
+
+                    except Exception as ex:
+                        if str(ex) == 'wallclock and GPS clocks in significant disagreement':
+                            file_behavior = 'Disagreement'
+                            pass
+                        else:  # Mostly here so that if the reader ever runs into other errors I'll know about them
+                            print(f'line {ex.__traceback__.tb_lineno}:')
+                            print(f'{type(ex).__name__}: {ex}')
+                            exit()
+
+                    # Determines the time gaps between adjacent files
+                    first_second = filetimes[0]
+                    file_time_gap = first_second - last_second if files_imported > 0 else 0
+                    file_time_gaps = np.append(file_time_gaps, file_time_gap)
+                    last_second = filetimes[-1]
+                    files_imported += 1
+
+                    print(f'{files_imported}/{len(filelist)} files imported', end='\r')
+                    print(f'{day_files}|{file_behavior}|{file_time_gap}', file=datetime_logs)
+
+                # Makes the final arrays and exports them
+                scintillator.update({'filelist': filelist})
+                scintillator.update({'time': np.concatenate(time_list)})
+                scintillator.update({'energy': np.concatenate(energy_list)})
+                self.scintillators.update({i: scintillator})
+
+                print(f'\nTotal Counts: {len(np.concatenate(time_list))}', file=datetime_logs)
+                print(f'Average time gap: {np.sum(file_time_gaps) / len(file_time_gaps)}\n', file=datetime_logs)
+            except (AssertionError, ValueError):
+                print('Missing data for the specified day.', file=datetime_logs)
+                print('Missing data for the specified day.', end='\r')
+                continue
+
+
+class ShortEvent:
+    def __init__(self, event_start, event_length, scintillator):
+        self.start = int(event_start)
+        self.length = int(event_length)
+        self.stop = int(event_start + event_length)
+        self.scintillator = scintillator
+
+    # Makes the short event scatter plots
+    def scatterplot_maker(self, timescales, filelist, times, energies, event_number, date_timestamp, unit, mode):
+        # so many arguments
+        event_times = times[self.start:self.stop]
+        event_energies = energies[self.start:self.stop]
+        event_length = event_times[-1] - event_times[0]
+
+        figure1 = plt.figure(figsize=[20, 11.0])
+        figure1.suptitle(f'Event {str(event_number)} for {date_timestamp}, {len(event_energies)} counts',
+                         fontsize=20)
+        ax1 = figure1.add_subplot(3, 1, 1)
+        ax2 = figure1.add_subplot(3, 1, 2)
+        ax3 = figure1.add_subplot(3, 1, 3)
+        ax_list = [ax1, ax2, ax3]
+
+        for v in range(len(ax_list)):
+            ts = timescales[v]
+            ax = ax_list[v]
+            padding = (ts - event_length) / 2
+            if event_length >= ts:
+                best_time = event_times[np.argmin(np.abs(event_times - np.roll(event_times, 1)))]
+                ax.set_xlim(xmin=best_time - (ts / 2), xmax=best_time + (ts / 2))
+            else:
+                ax.set_xlim(xmin=event_times[0] - padding, xmax=event_times[-1] + padding)
+            dot_size = 3 if ts == timescales[0] else 1  # makes larger dots for top plot
+            ax.set_yscale('log')
+            ax.set_ylim([0.5, 1e5])
+            ax.scatter(times, energies + 0.6, s=dot_size, zorder=1)
+            ax.set_xlabel(f'Time (Seconds, {ts}s total)')
+            ax.set_ylabel('Energy Channel')
+            # Lines appear (100*percent)% to the left or right of event start/stop depending on subplot timescale
+            percent = 0.001
+            ax.vlines([event_times[0] - percent*ts, event_times[-1] + percent*ts], 0, 1e5,
+                      colors='r', linewidth=1, zorder=-1, alpha=0.75)
+
+        # Adds the name of the relevant data file to the scatter plot
+        event_time = times[self.start]
+        event_file = ''
+        eventfound = False
+        files_examined = 0
+        new_filelist = []
+        for day_files in filelist:
+            if mode == 'processed':
+                e, temp_t_array = np.loadtxt(day_files, skiprows=1, usecols=(0, 2), unpack=True)
+            else:
+                try:
+                    data = dr.fileNameToData(day_files)
+                except Exception as ex:
+                    if str(ex) == 'wallclock and GPS clocks in significant disagreement':
+                        pass
+                    else:  # Mostly here so that if the reader ever runs into other errors I'll know about them
+                        print(f'line {ex.__traceback__.tb_lineno}:')
+                        print(f'{type(ex).__name__}: {ex}')
+                        exit()
+
+                temp_t_array = data['SecondsOfDay'].to_numpy()
+
+            for j in temp_t_array:
+                if j == event_time:
+                    event_file = day_files
+                    eventfound = True
+                    break
+
+            if eventfound:
+                new_filelist = filelist[files_examined:]
+                break
+
+            files_examined += 1
+        plt.title(f'Obtained from {event_file}', fontsize=15, y=-0.4)
+
+        # Saves the scatter plot
+        full_day_string = f'{str(date_timestamp)[2:4]}{str(date_timestamp)[5:7]}{str(date_timestamp)[8:10]}'  # Lazy fix
+        scatterpath = f'{sm.results_loc()}Results/{unit}/{full_day_string}/scatterplots/'
+        sm.path_maker(scatterpath)
+        figure1.savefig(f'{scatterpath}{full_day_string}_{self.scintillator}_event{event_number}.png')
+        plt.close(figure1)
+        return new_filelist
+
+
+class PotentialGlow:
+    def __init__(self, glow_start, glow_length):
+        self.start = int(glow_start)
+        self.length = int(glow_length)
+        self.stop = int(glow_start + glow_length - 1) if self.length > 1 else int(glow_start + glow_length)
+        self.peak_index = 0
+
+    def highest_zscore(self, z_scores):
+        glow_scores = z_scores[self.start:self.stop]
+        highest_score = np.max(glow_scores)
+        self.peak_index = np.argmax(glow_scores) + self.start
+        return highest_score
+
+    def glow_length_and_beginning_seconds(self, bins10sec):
+        glow_times = bins10sec[self.start:self.stop]
+        first_sec = glow_times[0]
+        length = self.length * 10
+        return first_sec, length
---
+++ search_module.py
@@ -0,0 +1,247 @@
+import os
+import numpy as np
+import math as math
+import matplotlib.pyplot as plt
+from scipy import signal
+
+# Functions used in search.py. Also, a centralized location for file paths and detector locations
+
+# Import/export paths: by the convention I've chosen, there should ALWAYS be a slash at the end of these strings
+
+
+# Info functions:
+# Custom raw data location
+def C_raw_data_loc():
+    # Enter the custom raw data filepath inbetween the quotes. Remember to include a SLASH at the end
+    path = ''
+    return path
+
+
+# Location where results from search.py are exported (same as program location by default)
+def results_loc():
+    path = ''
+    return path
+
+
+# Location of GODOT raw data
+def G_raw_data_loc():
+    path = '/media/AllDetectorData/Detectors/GODOT/Data/'
+    return path
+
+
+# Location of THOR raw data
+def T_raw_data_loc():
+    path = '/media/AllDetectorData/Detectors/THOR/'
+    return path
+
+
+# Location of Santis instrument raw data
+def S_raw_data_loc():
+    path = '/media/AllDetectorData/Detectors/SANTIS/Data/'
+    return path
+
+
+# Location of GODOT processed data
+def G_processed_data_loc():
+    path = '/media/godot/godot/monthly_processed/'
+    return path
+
+
+# THOR eRC values:
+def T_eRC(unit):
+    # all lists are in this form: NaI, small plastic, medium plastic, large plastic
+    THOR1 = ['4179', '4194', '4189', '4195']
+    THOR2 = ['4182', '4172', '4167', '4187']
+    THOR3 = ['4169', '4175', '4174', '4185']
+    THOR4 = ['4177', '4191', '4192', '4181']
+    THOR5 = ['4188', '4190', '4178', '4173']
+    THOR6 = ['4186', '4176', '4183', '4180']
+
+    THOR_dict = {'THOR1': THOR1, 'THOR2': THOR2, 'THOR3': THOR3, 'THOR4': THOR4, 'THOR5': THOR5, 'THOR6': THOR6}
+    return THOR_dict[unit]
+
+
+# Detector Locations. To add another detector/location simply add another if/elif statement
+def location(unit, YEAR):
+    if unit == 'GODOT':
+        if YEAR == 2015:
+            location = 'at HAWC, Mexico until September, then at Uchinada'
+        elif YEAR == 2016:
+            location = 'at HAWC, Mexico'
+        elif YEAR == 2017:
+            location = 'at HAWC, Mexico'
+        elif YEAR == 2018:
+            location = 'at Uchinada, Japan'
+        elif YEAR == 2019:
+            location = 'at Uchinada, Japan'
+        elif YEAR == 2020:
+            location = 'at Uchinada, Japan'
+        else:  # YEAR == 2021-20xx
+            location = 'at Uchinada, Japan'
+
+    elif unit[0:4] == 'THOR':  # Temporary. Eventually this will contain a list of all THOR unit locations as well
+        if unit == 'THOR5':
+            location = 'at mount Fuji, Japan'  # temp
+        else:
+            location = 'at no listed location'
+
+    elif unit == 'SANTIS':
+        location = 'Santis, Switzerland'
+    else:
+        location = 'at no listed location'
+
+    return location
+
+
+# Returns the number of days in the requested month
+def days_per_month(month, year):
+    month_dict = {'1': 31,  # January
+                  '2': 29 if year % 4 == 0 or (year % 100 != 0 and year % 400 == 0) else 28,  # February
+                  '3': 31,  # March
+                  '4': 30,  # April
+                  '5': 31,  # May
+                  '6': 30,  # June
+                  '7': 31,  # July
+                  '8': 31,  # August
+                  '9': 30,  # September
+                  '10': 31,  # October
+                  '11': 30,  # November
+                  '12': 31}  # December
+    return month_dict[str(month)]
+
+
+# Program functions:
+# Prints the specified string to both the console and the log
+def print_logger(string, logfile):
+    print(string)
+    print(string, file=logfile)
+
+
+# Checks to see if a given path exists and, if not, creates it
+def path_maker(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+# Uses compton edges obtained from scintillator calibration to convert energy array values into actual energies in MeV
+def channel_to_mev(energy_array, channel1, channel2, eRC):  # review this
+    if eRC == '1491':
+        K40 = 9.982624235104672506e-13  # Compton edge photon wavelength for Potassium 40 (meters)
+        T = 5.207231961360773518e-13  # Compton edge photon wavelength for Thorium (meters)
+    else:
+        K40 = 8.492068013698631405e-13  # Photo-peak photon wavelength for Potassium 40 (meters)
+        T = 4.768622807692308078e-13  # Photo-peak photon wavelength for Thorium (meters)
+    h = 4.1357e-21  # Planck's constant in MeV*s
+
+    a = (K40 - T) / (channel1 - channel2)
+    b = K40 - (channel1 * (K40 - T)) / (channel1 - channel2)
+    energy_array = (h * 2.998e8) / (a * energy_array + b)
+    return energy_array
+
+
+# Converts date to Julian date
+def date_to_julian(yr, mnth, dy):  # review this
+    if mnth == 1 or mnth == 2:
+        yearp = yr - 1
+        monthp = mnth + 12
+    else:
+        yearp = yr
+        monthp = mnth
+
+    # Checks where we are in relation to October 15, 1582, the beginning of the Gregorian calendar
+    if (yr < 1582) or (yr == 1582 and mnth < 10) or (yr == 1582 and mnth == 10 and dy < 15):
+        # before start of Gregorian calendar
+        B = 0
+    else:
+        # after start of Gregorian calendar
+        A = math.trunc(yearp / 100.)
+        B = 2 - A + math.trunc(A / 4.)
+
+    if yearp < 0:
+        C = math.trunc((365.25 * yearp) - 0.75)
+    else:
+        C = math.trunc(365.25 * yearp)
+    D = math.trunc(30.6001 * (monthp + 1))
+    jd = B + C + D + dy + 1720994.5
+    return jd
+
+
+# Makes the flagged z-score subplots for the glow search histograms
+def hist_subplotter(ax, glow, times, bins10sec, mue, sigma):
+    padding = 20
+    c = 0 if (glow.peak_index - padding) < 0 else (glow.peak_index - padding)
+    d = 8639 if (glow.peak_index + padding) > 8639 else (glow.peak_index + padding)
+
+    subbins = bins10sec[c:d]
+    ax.hist(times, bins=subbins, alpha=0.5, color='c')
+    ax.set_xlabel('Seconds of Day (UT)')
+    ax.set_ylabel('Counts/10-sec', )
+    ax.axhline(y=(mue + 5 * sigma), color='blue', linestyle='dashed', linewidth=2)
+    ax.grid(True)
+
+
+# Makes the energy spectra histograms for each GODOT scintillator
+def g_spectra_maker(energies, date_timestamp, full_day_string, detector, eRC):  # review this
+    plt.figure(figsize=[20, 11.0])
+    energy_bins = np.linspace(0.0, 15008.0, num=939)
+    energy_hist, bin_edges = np.histogram(energies, bins=energy_bins)
+    flagged_indices = np.array([])
+
+    if eRC == '1491':
+        scintillator = 'Large Plastic'
+        window_size = 19
+        poly_order = 2
+        curve_change = 1000
+        # Makes a rough "response function" for plotting purposes.
+        smoothed_energies = signal.savgol_filter(energy_hist, window_size, poly_order)  # Savitzky-Golay filter
+        plt.plot(energy_bins[0:938], smoothed_energies, color='green', alpha=0.75)
+
+        # Finds all the points on the response function where the concavity changes from positive to negative
+        second_derivative = signal.savgol_filter(energy_hist, window_size, poly_order, 2)
+        curvature_sign = np.sign(second_derivative)
+        for jk in range(len(second_derivative)):
+            if jk > 0 and curvature_sign[jk] == -1 and curvature_sign[jk - 1] in [0, 1]:
+                lower_limit = energy_hist[jk - int(((window_size - 1) / 2))]
+                upper_limit = energy_hist[jk + int(((window_size - 1) / 2))]
+                # Only flags those points whose surrounding curvature is sufficiently hilly
+                if np.abs(lower_limit - upper_limit) > curve_change:
+                    flagged_indices = np.append(flagged_indices, jk)
+        if len(flagged_indices) < 2:
+            raise Exception('One or more compton edges not found')
+
+        if len(flagged_indices) > 2:
+            raise Exception('Too many concavity changes found')
+
+    else:
+        scintillator = 'Sodium Iodide'
+        # Takes the sum of each bin with its two closest neighboring bins on either side
+        sums = energy_hist
+        for i in range(2):
+            sums += (np.roll(energy_hist, i+1) + np.roll(energy_hist, i-1))
+
+        # Looks for the location of the maximum sum within the two bands where the peaks are likely to be
+        band_starts = [38, 94]
+        band_ends = [75, 125]
+        for th in range(len(band_starts)):
+            band_max = np.argmax(sums[band_starts[th]:band_ends[th]]) + int(band_starts[th])
+            flagged_indices = np.append(flagged_indices, band_max)
+
+    # Plots the actual spectrum
+    plt.title(f'Energy Spectrum for {scintillator}, {str(date_timestamp)}', loc='center')
+    plt.xlabel('Energy Channel')
+    plt.ylabel('Counts/bin')
+    plt.yscale('log')
+    plt.hist(energies, bins=energy_bins, color='r', rwidth=0.5, zorder=1)
+
+    # Saves energy bins corresponding to the desired energies and plots them as vertical lines
+    energy1 = energy_bins[int(flagged_indices[0])]
+    energy2 = energy_bins[int(flagged_indices[1])]
+    plt.vlines(energy_bins[flagged_indices.astype(int)], 0, 1e6, zorder=2, alpha=0.75)
+
+    # Saves the figure
+    sp_path = f'{results_loc()}Results/{detector}/{full_day_string}/'
+    path_maker(sp_path)
+    plt.savefig(f'{sp_path}{scintillator}_Spectrum.png', dpi=500)
+    plt.clf()
+
+    return energy1, energy2
