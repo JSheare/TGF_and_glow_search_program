7ff4ef5cd1655fc229f640abd03551e3026b3ece
JSheare <jrsheare@ucsc.edu>
2023-03-02 11:58:52 -0800

    Updated data reader

--- DataReaderFinal.py
+++ DataReaderFinal.py
@@ -1,4 +1,13 @@
+#Version 230226
+#Changed sign of unixtimecorrection to subtraction in processdatatiming()
+#Changed location of best time tag string in thor data to lines[i-4] (first tag) in thorFileToData()
+#Added ucsc_timestring_to_datetime() routine to correctly pad microseconds in UCSC time tags with 0s on the left,
+#         needed by datetime.strptime()
+#Added multifilesToData() routine to read multiple files into a single dataset
+#Added warnings about unexpected behavior across frame boundaries to thorFileToData()
+
 import re # Regular expressions module for string operations
+import pdb  #syntax to use is pdb.set_trace() for equivalent to an IDL "stop"
 import json
 import pandas as pd
 import glob
@@ -8,7 +17,29 @@ import scipy
 from scipy.stats import poisson
 from datetime import datetime
 import gzip
+from matplotlib import pyplot as plt
 
+def multifilesToData(filefile):
+    f=open(filefile,'r')
+    lines = f.read().splitlines()
+    started=0
+    for s in lines:
+        print("using file: "+s)
+        data = fileNameToData(s)
+        if (started==0):
+            alldata = data
+            started=1
+        else:
+            alldata=pd.concat([alldata,data],axis=0)
+    return(alldata)
+
+def ucsc_timestring_to_datetime(timeString):
+    parts = timeString.split()
+    microsec = parts[-1]
+    microsec = microsec.rjust(6,'0')
+    newString = parts[0]+' '+parts[1]+' ' +parts[2]+' '+parts[3]+' '+parts[4]+' '+parts[5]+' '+microsec
+    dateTime = datetime.strptime(newString, "%Y %m %d %H %M %S %f")
+    return(dateTime)
 
 def fileNameToData(fileName):
     if(fileName[-2:] == 'gz'):        # This decompresses the .gz file if it's gz, that way you don't have to uncompress a file first if you don't want to. Works either way.
@@ -33,10 +64,25 @@ def thorFileToData(lines):
     dataList = list()
     for i in range(5, len(lines), 6):
         lmString = lines[i]
-        timeString = lines[i - 2] #NEED TO DOUBLE CHECK THIS WITH JEFF
-        dateTime = datetime.strptime(timeString, "%Y %m %d %H %M %S %f")
+        timeString = lines[i - 4] #John had this wrong (he had i-2, which = 3rd stamp)
+        dateTime = ucsc_timestring_to_datetime(timeString)
+
         data = getDataFromLMthor(lmString)
         data = processDataTiming(data, dateTime, mode=0)
+        tarr = np.array(data['SecondsOfDay'])
+        tfirst = tarr[0]
+        tlast = tarr[-1]
+ 
+        if (i > 5):
+            dt=tfirst-tlast_prev
+            if (dt > 0.5):
+                #print('Long gap (>0.5s) between frames at: ',tfirst)
+                a = 1
+            if (dt < 0.0):
+                #print('Anomalous clock backwards at: ',tfirst,tlast_prev, dt)
+                a = 1
+
+        tlast_prev = tlast
         dataList.append(data)
     data = pd.concat(dataList)
     return(data)
@@ -51,11 +97,12 @@ def lmFileToData(lines, mode):
     elif mode is 2:
         start = 7
         increment = 6
-        tStamp = 2 #NEED TO DOUBLE CHECK THIS WITH JEFF
+        tStamp = 0 #NEED TO DOUBLE CHECK THIS WITH JEFF (was 2 -- experimenting)
     for i in range(start, len(lines), increment):
         lmString = lines[i]
         timeString = lines[i + tStamp]
-        dateTime = datetime.strptime(timeString, "%Y %m %d %H %M %S %f")
+        dateTime = ucsc_timestring_to_datetime(timeString)
+        
         data = getDataFromLM(lmString, mode)
         if (i + increment < len(lines)):
             nextLmString = lines[i + increment]
@@ -79,13 +126,16 @@ def getDataFromLMthor(lmString):
 
 def processDataTiming(data, dateTime, mode):
     data['Seconds'] = getSecondsFromWallClock(data, mode)
-    data['UnixTime'] = dateTime.timestamp() + data['Seconds'] - data['Seconds'].iloc[-1] ## this assigns dateTime to the final photon in the table, and works backwards from there to previous photons
+    data['UnixTime'] = dateTime.timestamp() + data['Seconds'] - data['Seconds'].iloc[-1] ## this assigns dateTime to the final photon in the table, and works 
+                                                                                         ##backwards from there to previous photons
     firstDT = datetime.fromtimestamp(data['UnixTime'].min())
-    data['SecondsOfDay'] = data['UnixTime'] - datetime(firstDT.year, firstDT.month, firstDT.day).timestamp() ## subtracts out 00:00:00 of the earliest day in UNIX time. For files that cross midnight this will result in SecondsOfDay exceeding 86000 or whatever
+    data['SecondsOfDay'] = data['UnixTime'] - datetime(firstDT.year, firstDT.month, firstDT.day).timestamp() ## subtracts out 00:00:00 of the earliest day in UNIX time. 
+                                                                              ##For files that cross midnight this will result in SecondsOfDay exceeding 86400
     if(data['gpsSync'].all()):
         unixTimeCorrection = data[data['PPS'] == 1]['SecondsOfDay'].apply(lambda x: (x - round(x))).median()
-        data['SecondsOfDay'] = data['SecondsOfDay'] + unixTimeCorrection
-        data['UnixTime'] = data['UnixTime'] + unixTimeCorrection ## some of this precision is going to get truncated. If that's a big deal then you need to recreate a DateTime object by adding the adjusted SecondsOfDay to firstDT.
+        data['SecondsOfDay'] = data['SecondsOfDay'] - unixTimeCorrection
+        data['UnixTime'] = data['UnixTime'] - unixTimeCorrection ## some of this precision is going to get truncated. If that's a big deal 
+                                                                 ##then you need to recreate a DateTime object by adding the adjusted SecondsOfDay to firstDT.
     data = data.drop('Seconds', axis = 1)
     return(data)
 
@@ -96,9 +146,13 @@ def getSecondsFromWallClock(data, mode):
         rolloverLength = pow(2, 32)
     elif mode == 2:
         rolloverLength = pow(2, 48)
-    rolloverCorrection = (data['wc'].diff() < 0).cumsum() * rolloverLength  ## Every time you encounter the wallclock going backwards, assume it's rollover. Count how many times its rolled over and multiply that by 2^36 for each row's correction.
-    data['wc'] = data['wc'] + rolloverCorrection ## Changing the contents of the object passed in the function - there might be a better way to do this but it should work since Python passes by object reference
+    rolloverCorrection = (data['wc'].diff() < -rolloverLength/4.).cumsum() * rolloverLength  ## Every time you encounter the wallclock going significantly backwards (1/4 of range), 
+                                                                        ## assume it's rollover. Count how many times its rolled over and multiply that by 2^36 for each row's correction.
+    data['wc'] = data['wc'] + rolloverCorrection ## Changing the contents of the object passed in the function - there might be a better way to do this but it should work since 
+                                                 ##Python passes by object reference.
+
     wallClockCorrection = (data['wc'][data['PPS'] == 1]).diff().median()
+
     if(abs(wallClockCorrection - 8e7) > 1e4):
         raise Exception("wallclock and GPS clocks in significant disagreement")
     if(np.isnan(wallClockCorrection)):
