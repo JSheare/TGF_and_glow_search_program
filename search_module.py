8f5a48ab2cf3d748f1f973de0ad8af7bc5c41d2f
JSheare <jrsheare@ucsc.edu>
2023-09-25 15:13:49 -0700

    Updated weather scraper to account for DST when applicable

--- search_module.py
+++ search_module.py
@@ -39,11 +39,15 @@ Functions:
     channel_to_mev:
         Converts all the values of an array containing photon energies from channel voltage to MeV based on two
         conversion factors obtained from the associated scintillator's calibration.
-    utc_to_local_date:
-        Converts the detector date to the correct local one based on the time of the supplied event.
+    dst_status:
+        Returns string statuses depending on whether a day falls inside/outside/on the edge of dst.
+    dst_conversion:
+        Returns an updated utc to local conversion number depending on the given date and time.
+    convert_to_local:
+        Converts the detector date and event time to what they would actually be in local time.
     scrape_weather:
         Scrapes weather from weather underground and returns the results as a pandas data frame.
-    convert_timestamp:
+    convert_clock_hour:
         Converts a timestamp of the form hh:mm AM/PM into seconds since the beginning of the day.
     get_weather:
         Scrapes weather underground and returns the weather at the approximate time of an event.
@@ -53,6 +57,7 @@ Functions:
 import os as os
 import contextlib as contextlib
 import pickle as pickle
+import datetime as dt
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -270,22 +275,101 @@ def channel_to_mev(energy_array, channel, scintillator):
     return energy_array
 
 
-def utc_to_local_date(detector, event_time):
-    """Converts the detector date to the correct local one based on the time of the supplied event."""
-    utc_conversion = int(detector.location['UTC_conversion'])
+def dst_status(full_day_string):
+    """Returns string statuses depending on whether a day falls inside/outside/on the edge of dst."""
+    century = '20'
+    year = int(century + full_day_string[0:2])
+    month = int(full_day_string[2:4])
+    day = int(full_day_string[4:])
 
+    # January, February, and December are never DST
+    if month < 3 or month > 11:
+        return 'outside'
+    # April to October are always DST
+    elif 3 < month < 11:
+        return 'inside'
+    # DST starts on the second Sunday of March (which is always between the 8th and the 14th)
+    elif month == 3:
+        second_sunday = 8 + (6 - dt.datetime(year, month, 8).weekday())
+        if day < second_sunday:
+            return 'outside'
+        elif day > second_sunday:
+            return 'inside'
+        else:
+            return 'beginning'
+    # DST ends on the first Sunday of November (so the previous Sunday must be before the 1st)
+    else:
+        first_sunday = 1 + (6 - dt.datetime(year, month, 1).weekday())
+        if day < first_sunday:
+            return 'inside'
+        elif day > first_sunday:
+            return 'outside'
+        else:
+            return 'end'
+
+
+def dst_conversion(full_day_string, event_time, utc_conversion):
+    """Returns an updated utc to local conversion number depending on the given date and time."""
+    two_am = 7200  # Number of seconds of the day corresponding to 2:00AM
+    sec_per_hour = 3600
+    sec_per_day = 86400
+
+    temp_time = event_time + (utc_conversion * sec_per_hour)
+    if temp_time > sec_per_day:
+        temp_time -= sec_per_day
+        temp_date = roll_date_forward(full_day_string)
+    elif temp_time < 0:
+        temp_time += sec_per_day
+        temp_date = roll_date_backward(full_day_string)
+    else:
+        temp_date = full_day_string
+
+    temp_date_status = dst_status(temp_date)
+    if temp_date_status == 'inside':  # Squarely inside dst
+        return utc_conversion + 1
+    elif temp_date_status == 'outside':  # Squarely outside dst
+        return utc_conversion
+    elif temp_date_status == 'beginning':  # Beginning of dst (2nd Sunday of March at 2:00AM)
+        if temp_time >= two_am:
+            return utc_conversion + 1
+        else:
+            return utc_conversion
+    else:  # End of dst (1st Sunday of November at 2:00AM)
+        if (temp_time + sec_per_hour) >= two_am:  # + sec_per_hour b/c temp time should be in dst
+            return utc_conversion
+        else:
+            return utc_conversion + 1
+
+
+def convert_to_local(detector, event_time):
+    """Converts the detector date and event time to what they would actually be in local time."""
     sec_per_hour = 3600
     sec_per_day = 86400
     century = '20'
 
-    if (event_time + (sec_per_hour * utc_conversion)) > sec_per_day:  # If the event happened the next day local time
+    # In case the event happened in the first ~300 seconds of next day included in the dataset
+    if event_time > sec_per_day:
         full_day_string = roll_date_forward(detector.full_day_string)
-    elif (event_time + (sec_per_hour * utc_conversion)) < 0:
-        full_day_string = roll_date_backward(detector.full_day_string)  # If the event happened the previous day lt
+        event_time -= sec_per_day
     else:
-        return detector.date_timestamp
+        full_day_string = detector.full_day_string
 
-    return f'{century}{full_day_string[0:2]}-{full_day_string[2:4]}-{full_day_string[4:]}'
+    utc_conversion = int(detector.location['UTC conversion to local time'])
+
+    # Corrects the UTC conversion if we're in daylight savings time
+    if detector.location['Daylight savings?'].lower() in ['yes', 'true']:  # IDK which one will end up being used
+        utc_conversion = dst_conversion(full_day_string, event_time, utc_conversion)
+
+    if (event_time + (sec_per_hour * utc_conversion)) > sec_per_day:  # If the event happened the next day local time
+        full_day_string = roll_date_forward(full_day_string)
+        event_time = (event_time + (sec_per_hour * utc_conversion)) - sec_per_day
+    elif (event_time + (sec_per_hour * utc_conversion)) < 0:
+        full_day_string = roll_date_backward(full_day_string)  # If the event happened the previous day local time
+        event_time = (event_time + (sec_per_hour * utc_conversion)) + sec_per_day
+    else:
+        event_time = event_time + (sec_per_hour * utc_conversion)
+
+    return f'{century}{full_day_string[0:2]}-{full_day_string[2:4]}-{full_day_string[4:]}', event_time
 
 
 def scrape_weather(date, station):
@@ -322,23 +406,12 @@ def scrape_weather(date, station):
         return None
 
 
-def convert_timestamp(timestamp):
-    """Converts a timestamp of the form hh:mm AM/PM into seconds since the beginning of the day.
+def convert_clock_hour(clock_hour):
+    """Converts a timestamp of the form hh:mm AM/PM into seconds since the beginning of the day."""
 
-    Parameters
-    ----------
-    timestamp : str
-        A string containing the timestamp to be converted (in the format hh:mm AM/PM).
-
-    Returns
-    -------
-    float
-        The number of seconds of the day corresponding to the timestamp.
-
-    """
-    meridiem = timestamp.split()[1]
-    hour = int(timestamp.split()[0].split(':')[0])
-    minute = int(timestamp.split()[0].split(':')[1])
+    meridiem = clock_hour.split()[1]
+    hour = int(clock_hour.split()[0].split(':')[0])
+    minute = int(clock_hour.split()[0].split(':')[1])
 
     # Converting from 12 hour time to 24 hour time
     if meridiem == 'AM' and hour == 12:  # midnight
@@ -369,9 +442,7 @@ def get_weather(detector, times, event):
         A string containing the weather conditions at the approximate time of the event.
 
     """
-    event_time = times[event.start] - 86400 if times[event.start] > 86400 else times[event.start]
-
-    date = utc_to_local_date(detector, event_time)
+    date, event_time = convert_to_local(detector, times[event.start])
     station = detector.location['nearest_station']
     weather_table = scrape_weather(date, station)
 
@@ -380,9 +451,9 @@ def get_weather(detector, times, event):
         index = 0
         best_diff = float('inf')
         best_index = 0
-        for time in weather_table['Time']:
-            if type(time) != float:
-                time_sec = convert_timestamp(time)
+        for clock_hour in weather_table['Time']:
+            if type(clock_hour) != float:
+                time_sec = convert_clock_hour(clock_hour)
                 time_diff = abs(event_time - time_sec)
                 if time_diff < best_diff:
                     best_diff = time_diff
