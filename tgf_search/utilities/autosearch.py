b77b4f425bc969d94f9ab1a70b36fcf022e18034
JSheare <jrsheare@ucsc.edu>
2024-02-20 13:37:44 -0800

    Minor restructuring and formatting update

--- tgf_search/utilities/autosearch.py
+++ tgf_search/utilities/autosearch.py
@@ -9,15 +9,17 @@ import platform as platform
 import tgf_search.tools as tl
 
 
-def search(detector, dates, data_path, results_path, autosearch_path):
+def search(unit, checked_dates, data_path, results_path, autosearch_path):
+    if unit not in checked_dates:
+        checked_dates[unit] = []
+
     queue = []
     days = glob.glob(f'{data_path}/*')
     days.sort()
-    already_checked = dates[detector]
     for day in days:
         if day[-6:].isnumeric():
             has_data = True if len(os.listdir(day)) > 0 else False
-            if has_data and day[-6:] not in already_checked:
+            if has_data and day[-6:] not in checked_dates[unit]:
                 queue.append(day[-6:])
 
     # Check the most recent stuff first
@@ -27,14 +29,14 @@ def search(detector, dates, data_path, results_path, autosearch_path):
     script_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '\\search.py'
     executable = 'python' if platform.system() == 'Windows' else 'python3'
     for day in queue:
-        if detector == 'THOR1':
+        if unit == 'THOR1':
             # Control flow for THOR1 NOAA flights
             if 220930 <= int(day) <= 230208:
                 command = [executable, script_path, day, day, 'THOR1', 'aircraft', 'skcali']
             else:
                 command = [executable, script_path, day, day, 'THOR1']
 
-        elif detector == 'SANTIS':
+        elif unit == 'SANTIS':
             # Control flow for SANTIS instrument becoming the CROATIA instrument
             if int(day) > 211202:
                 command = [executable, script_path, day, day, 'CROATIA']
@@ -42,7 +44,7 @@ def search(detector, dates, data_path, results_path, autosearch_path):
                 command = [executable, script_path, day, day, 'SANTIS']
 
         else:
-            command = [executable, script_path, day, day, f'{detector}']
+            command = [executable, script_path, day, day, f'{unit}']
 
         # Runs search.py for the day
         old_pwd = os.getcwd()
@@ -54,30 +56,30 @@ def search(detector, dates, data_path, results_path, autosearch_path):
 
         if result.wait() != 0:
             # Writes any errors that search.py runs into to text files so that they can be examined/fixed later
-            tl.make_path(autosearch_path + 'Error Logs')
-            with open(autosearch_path + f'Error Logs/{detector}_{day}_Error.txt', 'w') as error_file:
+            tl.make_path(f'{autosearch_path}/Error Logs')
+            with open(f'{autosearch_path}/Error Logs/{unit}_{day}_Error.txt', 'w') as error_file:
                 output, error = result.communicate()
                 error_file.write(str(error.strip().decode('utf-8')))
 
         else:
             # Adds the day to the list of checked dates
-            dates[detector].append(day)
+            checked_dates[unit].append(day)
 
             # Dumps the updated list of checked dates to a json file for use the next day
-            with open(autosearch_path + 'checked_dates.json', 'w') as date_file:
-                json.dump(dates, date_file)
+            with open(f'{autosearch_path}/checked_dates.json', 'w') as date_file:
+                json.dump(checked_dates, date_file)
 
-    return dates
+    return checked_dates
 
 
 def main():
     results_path = '/home/jacob/search'
-    autosearch_path = results_path + '/Autosearch/'
+    autosearch_path = results_path + '/Autosearch'
     tl.make_path(autosearch_path)
 
     # Checks to see if the program is already running (maybe the dataset it's checking is quite large or long)
     try:
-        with open(autosearch_path + 'pid.txt', 'r') as existing_pid_file:
+        with open(f'{autosearch_path}/pid.txt', 'r') as existing_pid_file:
             pid = int(existing_pid_file.readline())
             if psutil.pid_exists(pid):
                 exit()
@@ -86,18 +88,16 @@ def main():
 
     # Runs the program normally if it isn't running already
     except FileNotFoundError:
-        with open(autosearch_path + 'pid.txt', 'w') as pid_file:
+        with open(f'{autosearch_path}/pid.txt', 'w') as pid_file:
             pid_file.write(str(os.getpid()))
 
         try:
-            date_file = open(autosearch_path + 'checked_dates.json')
-            checked_dates = json.load(date_file)
-            date_file.close()
+            with open(f'{autosearch_path}/checked_dates.json', 'r') as date_file:
+                checked_dates = json.load(date_file)
+
         # If the list ever gets deleted by accident or something
-        # (mostly here because I don't want to put the list together by hand)
         except FileNotFoundError:
-            checked_dates = {'THOR1': [], 'THOR2': [], 'THOR3': [],
-                             'THOR4': [], 'THOR5': [], 'THOR6': [], 'GODOT': [], 'SANTIS': []}
+            checked_dates = {}
 
         # Running the main program on each of the detectors
 
@@ -134,7 +134,7 @@ def main():
                                results_path, autosearch_path)
 
         # Deletes the pid file
-        os.remove(autosearch_path + 'pid.txt')
+        os.remove(f'{autosearch_path}/pid.txt')
 
 
 if __name__ == '__main__':
